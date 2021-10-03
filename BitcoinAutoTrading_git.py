de9178bb3f3d3cd96747727842dbeb386a23825e
duhyunkang <71219615+duhyunkang@users.noreply.github.com>
2021-10-03 11:15:22 +0900

    Update BitcoinAutoTrading_git.py
    buy_result, sell_result에서 error 발생시, buy 또는 sell 명령이 실행 안되므로 error 문구에 대한 추가처리 해줌.
    에러 발생 시에도, 반복 실행되게끔 함
    daily_msg 부분도 수정(매일 아침 가동중임을 알림)

--- BitcoinAutoTrading_git.py
+++ BitcoinAutoTrading_git.py
@@ -60,8 +60,8 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")    # 날짜가 바뀌면 매일 아침 시작 알림 띄우기하자
 post_message(myToken,"#coin", "Autotrade start")
 
-buy_result = None       # 값을 초기화 해주고
-daily_msg = False
+# 값초기화
+buy_result, daily_msg, sell_result = None
 
 # 자동매매 시작
 while True:
@@ -71,27 +71,29 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)  # 9:00 + 1일
         ratio = get_buying_ratio("KRW-BTC", 0.02)
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
+        if start_time < now < end_time - datetime.timedelta(seconds=60):
             target_price = get_target_price("KRW-BTC", 0.5)
             ma5 = get_ma5("KRW-BTC")
             current_price = get_current_price("KRW-BTC")
-            daily_msg = False
+            daily_msg, sell_result = None, None     # 각 초기화해주고
             if target_price < current_price and ma5 < current_price:
                 budget = get_balance("KRW") * ratio         # 잔고에 구매 비중을 곱한 금액만큼만 살것임 (구매 예산)
-                if budget > 5000 and buy_result is None:      # 구매 예산이 5천원 이상이고, buy_result가 None일때만 매수
+                # 구매 예산이 5천 이상이고, buy_result가 None 또는 error 일때만
+                if budget > 5000 and (buy_result is None or 'error' in buy_result):
                     buy_result = upbit.buy_market_order("KRW-BTC", budget*0.9995)
                     post_message(myToken, "#coin", "BTC buy : " +str(buy_result))
 
         else:
-            post_message(myToken, "#coin", "autotrade on-going")    # 매일 아침 해당 메시지를 띄워주고 싶음
             btc = get_balance("BTC")
             buy_result = None       # buy에서 에러 발생시 다음날 되면 buy_result를 초기화 해줘야함. sell 구분 뒤에 있음 에러 났을 경우 안팔리므로 실행 안됨
-            if btc > 0.00008:           #
-                sell_result = upbit.sell_market_order("KRW-BTC", btc)    # 전체 매도하려면 0.9995 제외하면 될듯
+            # BTC가 5천원 이상이고, sell_result가 None 또는 error 일때만
+            if btc > 0.00008 and (sell_result is None or 'error' in sell_result):
+                sell_result = upbit.sell_market_order("KRW-BTC", btc)
                 post_message(myToken, "#coin", "BTC sell : " + str(sell_result))
+            # 프로그램 가동중임을 알리는 알람
             if daily_msg is None:
                 post_message(myToken, "#coin", "program is running")
-                daily_msg = True
+                daily_msg = "On"
         time.sleep(1)
     except Exception as e:
         print(e)
